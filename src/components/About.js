b65404925b7d7d15eb9431ff44eae05f11baaa99
Tarun Singh <ts424@snu.edu.in>
2024-12-18 04:21:22 +0530

    second

--- src/components/About.js
+++ src/components/About.js
@@ -38,7 +38,7 @@ function About() {
         <Accordion.Item style={myStyle} eventKey="0">
           <Accordion.Header>Accordion Item #1</Accordion.Header>
           <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat. Duis aute irure dolor in
